c28f19063d43e1d0e82e2dbc7b161fce0be9fd22
Bedanta Dey <deybedanta17@proton.me>
2023-02-20 15:23:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function showHead() {
+    var username = document.getElementById('mc-username').value;
+    var headDiv = document.getElementById('mc-head');
+    if (username.length > 0) {
+        headDiv.style.display = 'block';
+        headDiv.style.backgroundImage = `url('https://crafthead.net/helm/${username}/150.png')`;
+        document.getElementById('save-button').style.display = 'block';
+    }
+}
+
+function showSkull() {
+    var username = document.getElementById('mc-username').value;
+    var headDiv = document.getElementById('mc-head');
+    if (username.length > 0) {
+        headDiv.style.display = 'block';
+        headDiv.style.backgroundImage = `url('https://crafthead.net/avatar/${username}/150.png')`;
+        document.getElementById('save-button').style.display = 'block';
+    }
+}
+
+
+function saveImage() {
+  var headDiv = document.getElementById('mc-head');
+  var image = headDiv.style.backgroundImage.slice(5, -2);
+  window.open(image, '_blank');
+}
+
